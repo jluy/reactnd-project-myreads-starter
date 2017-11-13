a5bdc39c6eb0bc5b8392fc73c8bbe3acb1a781e7
jluy <jeffluy@gmail.com>
2017-11-13 13:37:43 -0800

    Major: Use remote server for bookcase state

--- src/Book.js
+++ src/Book.js
@@ -5,35 +5,36 @@ class Book extends Component {
     static propTypes = {
         cover_location: PropTypes.string.isRequired,
         title: PropTypes.string.isRequired,
-        author: PropTypes.string.isRequired,
+        authors: PropTypes.string.isRequired,
         cover_width: PropTypes.number,
-        cover_height: PropTypes.number
-    }
-
-    static defaultProps = {
-        cover_width: 128,
-        cover_height: 192
+        cover_height: PropTypes.number,
+        bookcase: PropTypes.object.isRequired,
+        bookshelf: PropTypes.string.isRequired
     }
 
     render() {
-        const { cover_location, title, author, cover_width, cover_height} = this.props
+        const { cover_location, title, authors, cover_width, cover_height, bookcase, bookshelf} = this.props
         return (
             <li>
                 <div className="book">
                     <div className="book-top">
-                        <div className="book-cover" style={{ width: cover_width, height: cover_height || 192, backgroundImage: `url(" ${cover_location}"` }}></div>
+                        <div className="book-cover"
+                             style={{ width: cover_width || 128, height: cover_height || 192, backgroundImage: `url(" ${cover_location}"`}}></div>
                         <div className="book-shelf-changer">
-                            <select>
+                            <select defaultValue={bookshelf}>
                                 <option value="none" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
+                                {
+                                    Object.keys(bookcase).map(shelf => (
+                                        <option key={shelf} value={shelf}>{bookcase[shelf].bookshelfTitle} </option>
+                                        )
+                                    )
+                                }
                                 <option value="none">None</option>
                             </select>
                         </div>
                     </div>
                     <div className="book-title">{title}</div>
-                    <div className="book-authors">{author}</div>
+                    <div className="book-authors">{authors}</div>
                 </div>
             </li>
         )
